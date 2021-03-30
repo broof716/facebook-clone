45b37612a5995aba6b9080be4a14ecc55f87ffe9
Bryan Roof <broof716@gmail.com>
2021-03-30 16:47:11 -0700

    Creating Facebook-Clone Application

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAwSz1yPuAPwwfzoFif2rb0oHGEAYetNOk",
+  authDomain: "facebook-clone-c4ebc.firebaseapp.com",
+  projectId: "facebook-clone-c4ebc",
+  storageBucket: "facebook-clone-c4ebc.appspot.com",
+  messagingSenderId: "786573736332",
+  appId: "1:786573736332:web:29f10ca76891626f173ea9"
+};
