d3b7be6126268d00da09d460c0357df1e9f367cb
Bryan Roof <broof716@gmail.com>
2021-03-30 17:34:04 -0700

    imported icons using material-ui into Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Flag, Home, Search, StorefrontOutlined, Subscriptions, SupervisedUserCircle } from '@material-ui/icons';
 import React from 'react'
 import '../styles/Header.css'
 
@@ -5,9 +6,28 @@ function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <img src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.png" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png" alt="" />
+      </div>
+      <div className="header__input">
+        <Search />
+        <input type="text" />
       </div>
       <div className="header__middle">
+        <div className="header__option">
+          <Home fontSize="large" />
+        </div>
+        <div className="header__option">
+          <Flag fontSize="large" />
+        </div>
+        <div className="header__option">
+          <Subscriptions fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlined fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircle fontSize="large" />
+        </div>
 
       </div>
       <div className="header__right">
