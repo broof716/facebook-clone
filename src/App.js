eef971c3e98091fbb565c7c63ebe54df1b82011d
Bryan Roof <broof716@gmail.com>
2021-03-30 17:14:46 -0700

    Creating Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Lets build the facebook clone!</h1>
+
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.png" alt="" />
+      </div>
+      <div className="header__middle">
+
+      </div>
+      <div className="header__right">
+
+      </div>
+      
+      
+    </div>
+  )
+}
+
+export default Header
