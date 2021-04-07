019ce3933da6a752ff9259acbe2b680f2bbd4a43
Bryan Roof <broof716@gmail.com>
2021-04-07 16:36:09 -0700

    adding post styling to page

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -10,8 +10,19 @@ function Feed() {
       <StoryReel />
       <MessageSender />
 
-      <Post />
-      <Post />
+      <Post
+        profilePic={"https://media-exp1.licdn.com/dms/image/C5603AQGxzq36_GgMYg/profile-displayphoto-shrink_200_200/0/1556770619827?e=1622678400&v=beta&t=udDg1O-hpLfEycFpaQYsJolGW8QWpDbKSXgiAcehfeM"}
+        message="Wow, this works!"
+        timestamp='This is a timestamp'
+        username='Bryan Roof'
+        image="https://hackernoon.com/hn-images/0*3C8jCZetH7jqSQG_.jpg"
+      />
+      <Post
+              profilePic={"https://media-exp1.licdn.com/dms/image/C5603AQGxzq36_GgMYg/profile-displayphoto-shrink_200_200/0/1556770619827?e=1622678400&v=beta&t=udDg1O-hpLfEycFpaQYsJolGW8QWpDbKSXgiAcehfeM"}
+              message="Wow, this works!"
+              timestamp='This is a timestamp'
+              username='Bryan Roof'
+      />
       <Post />
     </div>
   );
