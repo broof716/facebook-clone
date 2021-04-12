8ea06e4f344fbc73deabd14a2a08659895784ce2
Bryan Roof <broof716@gmail.com>
2021-04-12 15:16:34 -0700

    adding widgets to right side of page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Feed from './components/Feed';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Widgets from './components/Widgets';
 
 function App() {
   return (
@@ -11,8 +12,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
-        {/* Feed */}
-        {/* Widgets */}
+        <Widgets />
       </div>
     </div>
   );
